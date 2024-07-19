0af465e86e2ae0b054ac3ae94c36b1960182b69c
Abhisek Konar <konarabhisek@gmail.com>
2024-07-19 14:24:33 -0400

    Added command line support to pass filename and port number when launching HTTP server.

--- utils/host_geojson.py
+++ utils/host_geojson.py
@@ -1,13 +1,26 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from argparse import ArgumentParser 
+from functools import partial
+
+def argument_parser():
+    args = ArgumentParser()
+    args.add_argument('--filename', required=True, type=str)
+    args.add_argument('--port', default=8000, type=int)
+    
+    return args.parse_args()
 
 
-FILEPATH = '../data/geo_data/dashboard.geojson'
 class MapHTTPRequestHandler(BaseHTTPRequestHandler):
+    def __init__(self, filepath, *args, **kwargs):
+        self.host_file_path=filepath 
+        super().__init__(*args, **kwargs)
+
+
     def do_GET(self):
-        f = open(FILEPATH, "rb").read()
+        f = open(self.host_file_path, "rb").read()
         # Set the referrer policy header
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-type', 'applictaion/json')
         self.send_header('Referrer-Policy', 'no-referrer')  # Change 'no-referrer' to your desired policy
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
@@ -17,6 +30,9 @@ class MapHTTPRequestHandler(BaseHTTPRequestHandler):
         host, port = self.client_address[:2]
         #return socket.getfqdn(host)
         return host
+    
+
+
 def run_server(server_class=HTTPServer, handler_class=MapHTTPRequestHandler, port=8000):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
@@ -24,4 +40,6 @@ def run_server(server_class=HTTPServer, handler_class=MapHTTPRequestHandler, por
     httpd.serve_forever()
 
 if __name__ == '__main__':
-    run_server()
+    args = argument_parser()
+    handler = partial(MapHTTPRequestHandler, f'../data/geo_data/{args.filename}')
+    run_server(handler_class=handler, port=args.port)
