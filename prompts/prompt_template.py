bd908170d6ffa6768634fded5452a179a16114cd
Abhisek Konar <konarabhisek@gmail.com>
2024-08-01 11:00:07 -0400

    Added new prompt templates for natual language to pandas query chain.  Modified the router chain for a 4 way routing between chat, writing, db  lookup and RAG.

--- prompts/prompt_template.py
+++ prompts/prompt_template.py
@@ -13,6 +13,19 @@ ROUTER_PROMPT_TEMPLATE_2 = "Given an input, decide whether responding to it woul
                     Respond with a json with two keys. 'response' and 'explaination'. 'response' should either be 'qa', 'conv' or 'writing'."
 
 
+
+ROUTER_PROMPT_TEMPLATE_3 = "Given an input, decide whether responding to it would require fact based question answering capabilitesl, a part of a general conversation, \
+                    need to consult a table or require assistance with creative writing. \
+                    \n Here is the input \n\n {input}.\n\n Repond with one word: 'qa', 'conv','table', or 'writing'. \
+                    'qa', if you think it would require fact based question answering capabilities, \
+                    'conv', if you think it is a part of a regular conversation, \
+                    'table' if you think it would need to access tabular data, and \
+                    'writing' if you understand that \
+                    the input is requesting help with creative writing. \n \
+                    Explain your answer.\n \
+                    Respond with a json with two keys. 'response' and 'explaination'. 'response' should either be 'qa', 'conv', 'table' or 'writing'."
+
+
 CONV_PROMPT_TEMPLATE = "You are a helpful bot who can hold a polite conversation with a fellow human. \
                         You will be provided with a history of messages. Based on that you need to form a final \
                         response. Try not to be too wordy.\n\n \n\n \
@@ -60,6 +73,54 @@ Response:
    
 '''
 
+NL_TO_PANDAS_QUERY_TEMPLATE = '''
+You are a bot who specializes on converting nautral language to Pandas retrieval query.
+Pandas is a python library for database management.
+You are provided with a table schema with column names and their types. 
+================
+Table variable name: st.session_state.cumm_data_df
+Table schema: 
+    Unnamed: 0                            int64
+    name                                 object
+    type                                 object
+    longitude                           float64
+    latitude                            float64
+    watershed                            object
+    reservoir                            object
+    timestamp                             int64
+    datetime                             object
+    operational_status                    int64
+    capacity (mw)                         int64
+    head (m)                            float64
+    units                               float64
+    water_flow_rate                     float64
+    reservoir_level                     float64
+    total_energy_output                 float64
+    total_energy_output_predict_mean    float64
+    total_energy_output_predict_std     float64
+    co2_emissions                       float64
+    anomaly_total_energy_output           int64
+    anomaly_water_flow_rate               int64
+    anomaly_co2_emissions                 int64
+    anomaly_reservoir_level               int64
+    anomaly                               int64
+==================
+Your job is to figure out a pandas query to fetch the data requested by the user.
+Respond with a dictionary with the following keys : 'query'
+Your response should not contain anything else.
+Example 1:
+    User input: How many times have Beaumont had anomaly in total energy production before timestamp 10?
+Response:
+    "query" : "st.session_state.cumm_data_df[(st.session_state.cumm_data_df['name']=='Beaumont') & (st.session_state.cumm_data_df[anomaly_total_energy_output]==1) & (st.session_state.cumm_data_df['timestamp']<10)]"
+
+====
+
+User input : {input}
+
+Response : 
+'''
+
+
 RETRIEVE_REPHRASE_PROMPT = ChatPromptTemplate.from_messages([
 ('system',"Given the above conversation history and the latest user input, \
  your task is to ONLY REWRITE the user input that can used as a standalone question. Respond with a json with \
@@ -67,6 +128,7 @@ RETRIEVE_REPHRASE_PROMPT = ChatPromptTemplate.from_messages([
 MessagesPlaceholder(variable_name="chat_history"),
 ("user","{input}")
 ])
+
 
 DOCUMENT_CHAIN_PROMPT = ChatPromptTemplate.from_messages([
 ("system", "Answer the user's questions based on the context below. \
@@ -77,11 +139,13 @@ DOCUMENT_CHAIN_PROMPT = ChatPromptTemplate.from_messages([
 ("user","{input}"),
 ])
 
+
 RAG_PROMPT_TEMPLATE =  "Answer the user's questions based on the context provided.\n \
          If you don't know the answer just say you dont know. Do not try to come up with something. \n \
          Keep your answer brief and to the point. \n\n \
          The context: {context} \n \
          Human's question: {input}"
+
 
 EMAIL_PROMPT_TEMPLATE= "Help the user write an email based on the user query provided below. \n \
         User query: {input} \n"
