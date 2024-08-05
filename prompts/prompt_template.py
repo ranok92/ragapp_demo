ee35277b3b210f699fa23ee7bb7285c6a904e18a
Abhisek Konar <konarabhisek@gmail.com>
2024-08-05 15:56:32 -0400

    Optimizing prompts for more reliable performance.

--- prompts/prompt_template.py
+++ prompts/prompt_template.py
@@ -15,15 +15,17 @@ ROUTER_PROMPT_TEMPLATE_2 = "Given an input, decide whether responding to it woul
 
 
 ROUTER_PROMPT_TEMPLATE_3 = "Given an input, decide whether responding to it would require fact based question answering capabilitesl, a part of a general conversation, \
-                    need to consult a table or require assistance with creative writing. \
-                    \n Here is the input \n\n {input}.\n\n Repond with one word: 'qa', 'conv','table', or 'writing'. \
-                    'qa', if you think it would require fact based question answering capabilities, \
+                    need to consult a powerplant related table or require assistance with creative writing. \n \
+                    The table contains information regarding power plants within an electrical grid, \
+                    their names, locations, details regarding power generation and different anomalies pertaining to power plants. \
+                    \n Here is the input \n\n {input}.\n\n Repond with one word: 'power', 'conv','qa', or 'writing'. \
+                    'power' if you think it would need to access the powerplant tabular data, \
                     'conv', if you think it is a part of a regular conversation, \
-                    'table' if you think it would need to access tabular data, and \
+                    'qa', if you think it would require fact based question answering capabilities, and \
                     'writing' if you understand that \
                     the input is requesting help with creative writing. \n \
                     Explain your answer.\n \
-                    Respond with a json with two keys. 'response' and 'explaination'. 'response' should either be 'qa', 'conv', 'table' or 'writing'."
+                    Respond with a json with two keys. 'response' and 'explaination'. 'response' should either be 'power', 'conv', 'qa' or 'writing'."
 
 
 CONV_PROMPT_TEMPLATE = "You are a helpful bot who can hold a polite conversation with a fellow human. \
@@ -42,24 +44,23 @@ RAG_PROMPT_TEMPLATE =  "Answer the user's questions based on the context provide
 
 TABLE_SUMMARIZER_TEMPLATE = '''
 You are a bot who specializes on reading tabular data and summarizing the contents. \n
-The table data you will read belongs to a power grid system. Try to provide 
-suggestions as to why the anomaly occured along with the summary. \n\n Just provide your thoughts. 
+The table data you will read belongs to a power grid system. '1' under any column indicates the presence of the said entity, '0' indicates normal funtion. \n
+Try to provide suggestions as to why the anomaly occured along with the summary. \n\n Just provide your thoughts. 
 No need to ask for feedback. Always respond in third person.\\
 Respond with a dictionary with two keys: 'summary' and 'thoughts'
 ====
 Example 1:
 
 Table data:
-    name	type	watershed	capacity (mw)	head (m)	units	water_flow_rate	reservoir_level	total_energy_output	co2_emissions	anomaly_total_energy_output	anomaly_water_flow_rate	anomaly_co2_emissions	anomaly_reservoir_level	anomaly
-    18	Beauharnois	Run of river	Saint Lawrence	1906	24.39	38.0	3793.0	41.0	312.0	15.0	0	1	0	0	1
-    18	Bersimis-2	Run of river	Betsiamites	869	115.83	5.0	1816.0	39.0	109.0	21.5	1	0	0	0	1
-    18	Brisay	Reservoir	Caniapiscau	469	37.5	2.0	2384.0	52.0	58.0	8.0	1	0	0	0	1
-    18	Chute-Hemmings	Run of river	Saint-François	29	14.64	6.0	397.0	37.0	5.0	4.0	1	0	0	0	1
-
+'            name  timestamp  anomaly_total_energy_output  anomaly_water_flow_rate  anomaly_co2_emissions  anomaly_reservoir_level  anomaly\n
+122  Beauharnois        122                            0                        1                      0                        0        1\n
+123  Bersimis-2         122                            1                        0                      0                        0        1\n
+134  Brislay            122                            1                        0                      0                        0        1\n
+135  Chute-Hemmings     122                            0                        0                      1                        0        1
 Response:
     
-        'summary' : 'Plants Bersimis-2, Brislay and Chute-Hemmings are currently facing anomalies in terms of power generation. Plant Beauharnois is facing erronous water flow rates.\n', 
-        'thoughts': 'Four plants are currently generating energy way lower than its output. Plant Beauharnois has having higher that normal water flow rate and should be closely monitored. \n
+        'summary' : 'Plants Bersimis-2 and Brislay are currently facing anomalies in terms of power generation. Plant Beauharnois is facing erronous water flow rates. Plant Chute-Hemmings is experiencing higher than usual CO2 emissions.\n', 
+        'thoughts': 'Four plants are currently anomalies. Plant Beauharnois has having abnormal normal water flow rate and should be closely monitored. \n
                 So, an inspection of the turbines or other equipment might be necessary if this continues to happen.'
     
 
@@ -82,23 +83,7 @@ Table variable name: st.session_state.cumm_data_df
 Table schema: 
     Unnamed: 0                            int64
     name                                 object
-    type                                 object
-    longitude                           float64
-    latitude                            float64
-    watershed                            object
-    reservoir                            object
     timestamp                             int64
-    datetime                             object
-    operational_status                    int64
-    capacity (mw)                         int64
-    head (m)                            float64
-    units                               float64
-    water_flow_rate                     float64
-    reservoir_level                     float64
-    total_energy_output                 float64
-    total_energy_output_predict_mean    float64
-    total_energy_output_predict_std     float64
-    co2_emissions                       float64
     anomaly_total_energy_output           int64
     anomaly_water_flow_rate               int64
     anomaly_co2_emissions                 int64
@@ -109,13 +94,18 @@ Your job is to figure out a pandas query to fetch the data requested by the user
 Respond with a dictionary with the following keys : 'query'
 Your response should not contain anything else.
 Example 1:
-    User input: How many times have Beaumont had anomaly in total energy production before timestamp 10?
+    User query: How many times have Plant Beaumont had anomaly in total energy production before timestamp 10?
 Response:
-    "query" : "st.session_state.cumm_data_df[(st.session_state.cumm_data_df['name']=='Beaumont') & (st.session_state.cumm_data_df[anomaly_total_energy_output]==1) & (st.session_state.cumm_data_df['timestamp']<10)]"
+    "query" : "st.session_state.cumm_data_df[(st.session_state.cumm_data_df['name']=='Beaumont') & (st.session_state.cumm_data_df['anomaly_total_energy_output']==1) & (st.session_state.cumm_data_df['timestamp']<10)]"
+
+Example 1:
+    User query: "Describe the status of Plant Chelsea at timestamp 57?"
+Response:
+    "query" : "st.session_state.cumm_data_df[(st.session_state.cumm_data_df['name']=='Chelsea') & (st.session_state.cumm_data_df['timestamp']==57)]"
 
 ====
 
-User input : {input}
+User query : {input}
 
 Response : 
 '''
