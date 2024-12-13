26b7e4e26936a0ac35e500f58ef65a65ffd4e272
Abhisek Konar <konarabhisek@gmail.com>
2024-12-12 20:53:14 -0500

    Added method to calculate symmetric MAPE.

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,6 @@
 import json
 import ipdb 
+import numpy as np
 import pandas_geojson as pdg
 
 def get_key_val_from_llm_json_string(dict_text, key):
@@ -14,3 +15,30 @@ def str_to_dict(str_data):
     if str_data.strip()[-1]!='}':
         str_data = str_data +'}'
     return json.loads(str_data)
+
+
+def symmetric_mape(actual, predicted):
+    """
+    Symmetric Mean Absolute Percentage Error
+    Handles small values more robustly
+    
+    Args:
+        actual (array-like): True values
+        predicted (array-like): Predicted values
+    
+    Returns:
+        float: sMAPE value
+    """
+    actual = np.array(actual)
+    predicted = np.array(predicted)
+    
+    # Avoid division by zero
+    denominator = np.abs(actual) + np.abs(predicted)
+    
+    # Prevent division by zero by adding a small epsilon
+    denominator = np.where(denominator == 0, np.finfo(float).eps, denominator)
+    
+    smape = np.mean(2 * np.abs(actual - predicted) / denominator * 100)
+    print(actual, predicted, (2 * np.abs(actual - predicted) / denominator * 100))
+    
+    return smape
