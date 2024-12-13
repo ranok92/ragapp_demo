83a081178c8fcbefd74b292614ab58049407ebe6
Abhisek Konar <konarabhisek@gmail.com>
2024-12-12 22:35:08 -0500

    Removed commented out lines related to anomaly detection. Refactored the build_doc_assistant_tab method to use the function from general_assistant directory to build up the UI.

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -62,55 +62,8 @@ def build_doc_assistant_tab():
 ''',
     ):
         st.markdown(f'<h2 style="color: white;"> Assistant </h2>', unsafe_allow_html=True)
-    setup_llms_assistant()
-    setup_llm_chains_assistant()
-    load_vectordbs()    
-    load_hallucination_detector()
-
-    if "messages_gen_assist" not in st.session_state:
-        st.session_state.messages_gen_assist = []
-
-    col1, col2 = st.columns([0.3,0.7], gap="small")
-    with col1:
-        respose_gen_console =  st.container(height=350, border=True)
-        document_management_console =  st.container(height=550, border=True)
-
-
-    with respose_gen_console:
-        build_chatbot_params_console()
-
-    with col2:
-        if st.session_state.show_supporting_docs:
-            chat_window =  st.container(height=500,  border=False)  
-            context_display_console = st.container(height=400,  border=True)  
-        else:
-            chat_window =  st.container(height=900,  border=True)  
-
-
-    with chat_window:
-        st.chat_input(placeholder = 'Ask me anything: From writing emails to finding answers from documents. ', 
-                        on_submit=query_chain_general_assistant,
-                        key='current_input')
-
-        #display the chat history so far
-        if st.session_state.show_supporting_docs:
-            with st.container(height=400):
-                for msg in st.session_state.messages_gen_assist:
-                    st.chat_message(msg['speaker']).markdown(msg['content'])
-        else:
-            with st.container(height=800):
-                for msg in st.session_state.messages_gen_assist:
-                    st.chat_message(msg['speaker']).markdown(msg['content'])
-
-    #display the documents in the context used to come up with the answer
-
-    if st.session_state.show_supporting_docs:
-        with context_display_console:
-                build_context_display_window()
-
-    with document_management_console:
-        build_doc_management_console()
-
+    build_main_page_general_assistant()
+    
 #---- Build solar forecast tab
 
 def build_forecast_tab():
@@ -193,12 +146,6 @@ def main():
 
     # read csv from a URL
 
-    # get_data_anomaly()
-    # get_data_full_anomaly()
-    # # ----------------------------
-    # setup_llms_anomaly()
-    # setup_llm_chains_anomaly()
-
     st.title("FinanceGPT: Forecasting & Support")
 
 
@@ -233,57 +180,7 @@ def main():
 
             #---- SET UP THE PAGE STRUCTURE ---
 
-            # with grid_overview_tab:
-            #     #create the header container
-            #     header_container =  stylable_container(key='anomaly_header',
-            #                             css_styles=''' 
-            #                             {
-            #                                 text-align: center;
-            #                                 background: #4b6cb7;
-            #                                 color: white;
-            #                                 border-radius: 10px;
-            #                             }
-            #                             ''')
-            #     map_and_chat_container = st.container(height=700, border=False)
-            #     grid_overview_container = st.container(height=550, border=False)
-                
-            #     with header_container:
-            #         st.markdown("<h2 style= 'text-align: center; color: white;'> Anomaly Detection Dashboard</h2>", unsafe_allow_html=True)
-
-            #     with map_and_chat_container:
-            #         map_col, chat_col = st.columns([0.7, 0.3])
-
-            #         with map_col:
-            #             write_latest_update_time()
-            #             draw_realtime_map()
-            #         with chat_col:
-            #             st.markdown("<h2 style='text-align: center; color: #453030;'> Assistant </h2>", unsafe_allow_html=True)
-            #             build_chat_window_anomaly()
-
-
-            #     with grid_overview_container:
-
-            #         # create two columns for charts
-            #         fig_col1, fig_col2 = st.columns([0.7,0.3])
-            #         with fig_col1:
-            #             #line chart over dayc
-            #             #plot_historic_line_chart(historic_chart_kpi, df_historic_weekly_minmax)
-            #             plot_outage_occurance_linechart()
-            #         with fig_col2:
-            #             #barchart with instantaneous readings
-            #             write_outages()
-
-            #     # with grid_overview_row2.container(height=250, border=True):
-                    
-            #     #     anomaly_col, summary_col = st.columns(2, gap='small')
-
-            #     #     with anomaly_col:
-            #     #         write_anomalies()
-                        
-            #     #     with summary_col.container(height = 220, border=True):
-            #     #         #write_llm_summarization()
-            #     #         pass
-
+   
             with forecast_tab:
                 build_forecast_tab()
 
